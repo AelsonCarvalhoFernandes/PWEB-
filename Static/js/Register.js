7daeae66ff3cb249fcfe80fb4865db314a05cf6d
Kaike-Marcelo <kaikeemarceloo@gmail.com>
2023-11-29 18:18:04 -0300

    Atualizando front

---
+++ Static/js/Register.js
@@ -0,0 +1,114 @@
+function verifyCheckbox() {
+    var checkbox = document.getElementById('checkVendedor');
+    var labelRg_Cnpj = document.getElementById('labelRg');
+    var labelCpf_Cnpj = document.getElementById('labelCpf');
+
+    var form = document.querySelector('form');
+    var btnSubmit = document.getElementById('btnSubmit');
+
+    var h1 = document.querySelector('h1');
+    var inputs = document.querySelectorAll('input[type="text"], input[type="email"], input[type="password"]');
+    var linkLogin = document.querySelector('a');
+
+    if (checkbox.checked) {
+        labelRg_Cnpj.innerText = 'RG / CNPJ';
+        labelCpf_Cnpj.innerText = 'CPF / CNPJ';
+
+        form.style.boxShadow = '0px 0px 10px 0px #42ffff';
+        btnSubmit.style.backgroundColor = '#1e1e1e';
+
+        h1.innerText = 'CADASTRO | VENDEDOR';
+        h1.style.color = '#1e1e1e';
+        h1.style.textShadow = 'none';
+
+        inputs.forEach(input => {
+            input.style.border = '1px solid #1e1e1e';
+        });
+        linkLogin.style.color = '#1e1e1e';
+
+    } else {
+        labelRg_Cnpj.innerText = 'RG';
+        labelCpf_Cnpj.innerText = 'CPF';
+
+        form.style.boxShadow = '0px 0px 10px 0px #aa00ff';
+        btnSubmit.style.backgroundColor = '#aa00ff';
+
+        h1.innerText = 'CADASTRO | USUÁRIO';
+        h1.style.color = '#aa00ff';
+        h1.style.textShadow = '0px 0px 10px rgba(166, 0, 249, 0.399)';
+
+        inputs.forEach(input => {
+            input.style.border = '1px solid #aa00ff';
+        });
+        linkLogin.style.color = '#aa00ff';
+    }
+}
+
+
+function validationForm() {
+    var form = document.querySelector('.formRegister');
+    var password = document.getElementById('password').value;
+    var confirmPasswordInput = document.getElementById('confirm_password');
+    var confirmPassword = confirmPasswordInput.value;
+    var errorMessage = document.getElementById('errorMessage');
+
+    if (password !== confirmPassword) {
+        confirmPasswordInput.style.border = '1px solid red';
+        errorMessage.textContent = 'As senhas não coincidem!';
+        errorMessage.style.color = 'red';
+
+    } else {
+        errorMessage.textContent = '';
+        confirmPasswordInput.style.border = '1px solid #1e1e1e';
+
+        if (form.checkValidity()) {
+            form.submit();
+        } else {
+            form.reportValidity();
+        }
+    }
+}
+
+
+function formatCPF() {
+
+    var cpf = document.getElementById('cpf');
+
+    var cpfValue = cpf.value;
+
+    if (cpfValue.length === 3 || cpfValue.length === 7) {
+        cpf.value += '.';
+    } else if (cpfValue.length === 11) {
+        cpf.value += '-';
+    }
+}
+
+function formatRG() {
+
+    var rg = document.getElementById('rg');
+
+    var rgValue = rg.value;
+
+    if (rgValue.length === 2 || rgValue.length === 6) {
+        rg.value += '.';
+    } else if (rgValue.length === 10) {
+        rg.value += '-';
+    }
+}
+
+function allowNumbersOnly(event) {
+    // Obtém o código da tecla pressionada
+    var keyCode = event.which || event.keyCode;
+
+    // Permite apenas números inteiros (0-9) e a tecla "backspace" (8)
+    if ((keyCode < 48 || keyCode > 57) && keyCode !== 8) {
+        event.preventDefault();
+    }
+}
+
+function clearField(event, object) {
+    if (event.keyCode === 8) {
+        var object = document.getElementById(object.id);
+        object.value = '';
+    }
+}
--- Views/Register.php
+++ Views/Register.php
@@ -1,53 +1,75 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <link rel="stylesheet" href="../Static/css/global.css">
     <link rel="stylesheet" href="../Static/css/header.css">
     <link rel="stylesheet" href="../Static/css/register.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,100,0,200" />
+    <script src="../Static/js/Register.js"></script>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,100,0,200" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NZ - Cadastro</title>
 </head>
+
 <body>
 
-    <form class="maxWidth" action="register" method="post">
+    <form class="formRegister maxWidth" action="register" method="post">
 
-        <h1>CADASTRO</h1>
+        <h1>CADASTRO | USUÁRIO</h1>
         <br>
 
-        <label for="username">Username</label>
-        <input name="username" type="text" autofocus>
+        <label for="username" id="labelUsername">Username</label>
+        <input name="username" type="text" id="username" autofocus required>
         <br>
 
         <label for="email">Email</label>
-        <input name="email" type="email" name="" id="">
+        <input name="email" type="email" id="email" required>
         <br>
 
         <!-- Senha e Confirmar Senha -->
-        <div class="containerPassword">
+        <div class="containerLabels">
             <label for="password">Senha</label>
-            <input name="password" type="password">
-
             <label for="password">Confirme a Senha</label>
-            <input name="confirm_password" type="password">
         </div>
 
+        <div class="containerPassword">
+            <input name="password" id="password" type="password" required>
+            <input name="confirm_password" id="confirm_password" type="password">
+        </div>
+        <p class="error-message" id="errorMessage"></p>
+
         <br>
+        <div class="containerLabels">
+            <label id="labelRg" for="rg">RG</label>
+            <label id="labelCpf" for="cpf">CPF</label>
+        </div>
+
         <div class="containeIdentity">
-            <label for="rg">RG</label>
-            <input name="rg" type="text">
-            <br>
-            <label for="cpf">CPF</label>
-            <input name="cpf" type="text">
+            <input name="rg" id="rg" type="text" oninput="formatRG()" onkeydown="clearField(event, this)"
+                onkeypress="allowNumbersOnly(event)" maxlength="13" required>
+
+            <input name="cpf" id="cpf" type="text" oninput="formatCPF()" onkeydown="clearField(event, this)"
+                onkeypress="allowNumbersOnly(event)" maxlength=" 14" required>
         </div>
 
         <br>
         <label for="telefone">Telefone</label>
-        <input name="telefone" type="text">
+        <input name="telefone" id="telefone" type="text" onkeypress="allowNumbersOnly(event)" maxlength="15" required>
 
         <br>
-        <input type="submit" value="CADASTRAR">
+        <div class="checkUserVendedor">
+            <input type="checkbox" id="checkVendedor" class="checkVendedor" value="vendedor"
+                onchange="verifyCheckbox()">
+            <label for="checkVendedor" class="labelVendedor">Criar conta como Vendedor</label>
+        </div>
+
+        <br>
+        <button id="btnSubmit" name="btnSubmit" type="button" onclick="validationForm()">Salvar</button>
+        
+        <a href="">Já tenho uma conta</a>
     </form>
 </body>
+
 </html>
